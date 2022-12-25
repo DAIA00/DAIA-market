667ad9f56f0d3e1adb6868a0e0d2392c0d9c00a1
DAIA00 <ibarradaniel@gmail.com>
2022-12-25 18:40:47 -0500

    First commit

---
+++ src/main/java/com/market/DAIA/DaiaApplication.java
@@ -0,0 +1,13 @@
+package com.market.DAIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/DAIA/DaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.DAIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
